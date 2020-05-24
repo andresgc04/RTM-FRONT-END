a39a6fbde0368c4d8702e7793b317addab9cca40
hugo guerrero <andresgc1997@outlook.com>
2020-05-24 02:04:04 -0400

    Front-End-Xamarin

---
+++ RTM-FRONT-END/RTM-FRONT-END/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RTM_FRONT_END.Services;
+using RTM_FRONT_END.Views;
+
+namespace RTM_FRONT_END
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RTM-FRONT-END/RTM-FRONT-END/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RTM_FRONT_END.Models;
+
+namespace RTM_FRONT_END.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
